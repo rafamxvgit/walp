bb72f5b992ea8e7d8232c1418ec15d8b1a8a9072
rafamxv <rmxvmail@gmail.com>
2024-10-09 17:09:16 -0300

    já existe uma função que muda o background

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,24 @@
 package main
 
-import (
-	"fmt"
-	iomem "walp/ioMem"
-)
+import setwall "walp/setWall"
 
 func main() {
-	iomem.InitMemFile()
-	programMem := iomem.ReadProgMem()
+	setwall.Setwall("/home/rmxv/tudo/images/wallpapers/dark/john-fowler-03Pv2Ikm5Hk-unsplash.jpg")
+	/*programMem := iomem.GetProgMem()
 	for _, col := range programMem.Coleções {
 		fmt.Printf("%+v\n", col)
 	}
+	println()
+
+	col1 := programMem.Coleções[1]
+	col1.NextImage()
+	programMem.Coleções[1] = col1
+
+	iomem.WriteProgramMem(programMem)
+	programMem = iomem.GetProgMem()
+
+	for _, col := range programMem.Coleções {
+		fmt.Printf("%+v\n", col)
+	}
+	*/
 }
